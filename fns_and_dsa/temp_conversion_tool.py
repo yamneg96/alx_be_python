2d38c07fc80288a1d23011ae5e76fd8c01546a57
Yamlak Negash <yamlaknegash96@gmail.com>
2025-06-08 17:06:51 +0300

    Updated temperature conversion file

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,5 +1,5 @@
-FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5.0 / 9.0
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9.0 / 5.0
 
 def convert_to_celsius(fahrenheit):
     return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
@@ -14,10 +14,10 @@ def main():
 
         if unit == 'C':
             result = convert_to_fahrenheit(temp)
-            print(f"{temp}°C is {result}°F")
+            print(f"{temp:.1f}°C is {result:.1f}°F")
         elif unit == 'F':
             result = convert_to_celsius(temp)
-            print(f"{temp}°F is {result}°C")
+            print(f"{temp:.1f}°F is {result:.15f}°C")
         else:
             print("Invalid unit. Please enter 'C' for Celsius or 'F' for Fahrenheit.")
     except ValueError:
